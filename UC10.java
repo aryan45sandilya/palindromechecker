aa7004a349dcf4b1a938927c5773033427c1c162
ARYAN SANDILYA <aryan45sandilya@gmail.com>
2026-03-13 10:20:49 +0530

    Add files via upload

---
+++ UC10.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class UC10 {
+
+    public static boolean isPalindrome(String str) {
+
+        // Normalize string: remove spaces and convert to lowercase
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        // Compare characters from both ends
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Palindrome (ignoring spaces and case)");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
